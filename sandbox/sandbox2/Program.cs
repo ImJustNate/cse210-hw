dc933952f18ba4064e93655ac0aa8a584c8770d3
ImJustNate <natewelling03@gmail.com>
2024-09-18 17:12:01 -0600

    yay

---
+++ sandbox/sandbox2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace sandbox2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Howdy, potates!");
+        Console.ReadLine();
+        string answer = Console.ReadLine();
+    }
+}
