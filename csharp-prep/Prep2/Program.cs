7af092ce80625a8f759643adad37618e9a1c57b3
ImJustNate <natewelling03@gmail.com>
2024-09-21 19:19:45 -0600

    hey this works!

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is your grade percentage? ");
+        string grade_str = Console.ReadLine();
+        int grade_int = int.Parse(grade_str);
+
+
+        if (grade_int >= 90)
+        {
+            Console.WriteLine("Grade is A");
+            Console.WriteLine("You passed Yah!");
+        }
+        else if (grade_int >= 80)
+        {
+            Console.WriteLine("Grade is B");
+            Console.WriteLine("You passed Yah!");
+        }
+        else if (grade_int >= 70)
+        {
+            Console.WriteLine("Grade is C");
+            Console.WriteLine("You passed Yah!");
+        }
+        else if (grade_int >= 60)
+        {
+            Console.WriteLine("Grade is D");
+            Console.WriteLine("You failed, Try again next semester");
+        }
+        else
+        {
+            Console.WriteLine("Grade is F");
+            Console.WriteLine("You failed, Try again next semester");
+        }
+}
 }
