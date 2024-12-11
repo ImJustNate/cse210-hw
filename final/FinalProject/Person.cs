f00c9f3a1bf4c76b9aed6c9006d5357f9226f3fd
ImJustNate <156931228+ImJustNate@users.noreply.github.com>
2024-12-11 16:51:32 -0700

    removal of exes code

--- final/FinalProject/Person.cs
+++ final/FinalProject/Person.cs
@@ -1,6 +1,5 @@
 abstract class Person{
     public List<Card> _hand = [];
-    private int _losses = 0;
 
     public abstract void SelectHand(List<string> cardList);
     public abstract Card SelectCard( string selectedCard);
